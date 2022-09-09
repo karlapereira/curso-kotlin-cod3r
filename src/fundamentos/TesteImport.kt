3c5e93ed8e7512c9d9b7d845628713e8c56e86a6
Karla Pereira <karlapereira@gec.inatel.br>
2022-09-09 13:59:45 -0300

    imports use cases

---
+++ src/fundamentos/TesteImport.kt
@@ -0,0 +1,15 @@
+package fundamentos
+
+import fundamentos.pacoteA.simplesFuncao as funcaoSimples
+import fundamentos.pacoteA.Coisa
+import fundamentos.pacoteA.FaceMoeda.CARA
+import fundamentos.pacoteB.*
+
+fun main() {
+    kotlin.io.println(funcaoSimples("OK"))
+
+    val coisa = Coisa("bola")
+    println(coisa.nome)
+
+    println("${soma(2,3)} ${subtracao(4,6)}")
+}
---
+++ src/fundamentos/pacoteA/Mistura.kt
@@ -0,0 +1,9 @@
+package fundamentos.pacoteA
+
+fun simplesFuncao(texto: String): String{
+    return "Textp = $texto"
+}
+
+class Coisa(val nome: String)
+
+enum class FaceMoeda { CARA, COROA}
